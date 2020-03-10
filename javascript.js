1bff443cb3aa21a9728647f5d2118f16f8660781
Nazim Rizvic <nazim.rizvic@me.com>
2020-03-10 20:28:26 +1100

    function made that creates 16*16 grid using for loop, with nested for loop.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 
-const makeBox = document.createElement("div");
-makeBox.classList.add("gridBox");
+
+
 
 //document.getElementById("makeBox").style.width = "300px";
 
@@ -9,4 +9,31 @@ const gridContainer = document.querySelector("#gridContainer");
 
 
 
-gridContainer.appendChild(makeBox);
+
+
+
+/* The code that creates the divs with dom need to be looped with each iteration.
+If they are declared outside of the loop, and only the appendchild action is looped,
+this will simply replace the the initial div the was appended to the container.
+By creating element within the loop, it creates a brand new div for appending to 
+the container.
+*/
+
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++ ) {
+        let makeBox = document.createElement("div");
+        makeBox.classList.add("gridBox");
+        document.getElementById("gridContainer").appendChild(makeBox);
+        }
+        let lineBreak = document.createElement("br");
+        document.getElementById("gridContainer").appendChild(lineBreak);
+    }
+}
+
+createGrid();
+
+
+/*document.getElementById("gridContainer").appendChild(makeBox);
+document.getElementById("gridContainer").appendChild(lineBreak);
+gridContainer.createElement(makeBoxClone); */
