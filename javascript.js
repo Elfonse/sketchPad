7771e54011702ae3a1d81e345fda5b9daaf6f721
Nazim Rizvic <nazim.rizvic@me.com>
2020-03-10 19:52:07 +1100

    made single box for 16*16 grid

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+
+const makeBox = document.createElement("div");
+makeBox.classList.add("gridBox");
+
+//document.getElementById("makeBox").style.width = "300px";
+
+const gridContainer = document.querySelector("#gridContainer");
+
+
+
+gridContainer.appendChild(makeBox);
